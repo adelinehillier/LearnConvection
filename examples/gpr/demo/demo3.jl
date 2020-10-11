3d3c9ae8536e452e5ab61ad7a33de1d1ace56212
Adeline Hillier <adelineh2000@gmail.com>
2020-10-11 01:47:12 -0700

    SMP test

--- examples/gpr/demo/demo3.jl
+++ examples/gpr/demo/demo3.jl
@@ -138,3 +138,47 @@ kernel   = get_kernel(k, logγ, 0.0, distance)
 # animate the result, where 𝒢 is tested on 𝒟_test
 anim = animate_profile_and_model_output(𝒢, 𝒟_test)
 gif(anim, "all.gif")
+
+
+
+
+
+
+𝒟_train  = LearnConvection.Data.data(train, problem; D=16, N=N);
+
+x = 𝒟_train.x[1300]
+
+plott(x) = plot(x, collect(1:16), legend=false)
+p(x) = plot!(x, collect(1:16), legend=false)
+
+plott(x)
+
+
+y = copy(x)
+y[12]+=0.1
+y[13]+=0.05
+y[3]+=0.1
+y[4]+=0.1
+y[7]+=0.1
+
+myplot = plott(y)
+p(y)
+
+png(myplot, "myyy.png")
+
+r = copy(y)
+convective_adjust!(r)
+
+myplot2 = plott(r)
+png(myplot2, "myyy3.png")
+
+function convective_adjust!(x)
+    # remove negative gradients from temperature profile
+    for i in length(x)-2:-1:2
+        if x[i] > x[i+1]
+            if x[i-1] > x[i]; x[i] = x[i+1]
+            else; x[i] = (x[i-1]+x[i+1])/2
+            end
+        end
+    end
+end
--- examples/gpr/demo/demo6.jl
+++ examples/gpr/demo/demo6.jl
@@ -2,11 +2,13 @@
 using LearnConvection
 using Plots
 
-train = ["general_strat_4_profiles.jld2", "general_strat_32_profiles.jld2"]
-validate = ["general_strat_12_profiles.jld2", "general_strat_24_profiles.jld2"]
-test = ["general_strat_8_profiles.jld2", "general_strat_16_profiles.jld2", "general_strat_20_profiles.jld2", "general_strat_28_profiles.jld2"]
+## Extrapolation
 
-Qs = [2]
+train = ["general_strat_4_profiles.jld2", "general_strat_8_profiles.jld2", "general_strat_12_profiles.jld2", "general_strat_16_profiles.jld2"]
+validate = ["general_strat_20_profiles.jld2", "general_strat_24_profiles.jld2"]
+test = ["general_strat_28_profiles.jld2", "general_strat_32_profiles.jld2"]
+
+Qs = [1,2]
 problems = [
             Slack("KPP"; parameters=KPP.Parameters(), modify_predictor_fn=f),
             Slack("TKE"; parameters=TKEMassFlux.TKEParameters(), modify_predictor_fn=f),
@@ -14,7 +16,6 @@ problems = [
             Residual("TKE"; parameters=TKEMassFlux.TKEParameters(), modify_predictor_fn=f),
             Sequential("TKE"; parameters=TKEMassFlux.TKEParameters(), modify_predictor_fn=f),
             Sequential("KPP"; parameters=KPP.Parameters(), modify_predictor_fn=f),
-            Sequential("T"; modify_predictor_fn=f),
             Sequential("dT"; modify_predictor_fn=f),
 ]
 
@@ -35,22 +36,22 @@ for Q in Qs
     𝒢 = LearnConvection.GaussianProcess.model(𝒟_test; kernel=optimized_kernel)
     anim = animate_profile(𝒢, 𝒟_test)
 
-    gif(anim,"$(typeof(problem))_$(problem.type)_Q_$(Q)_interpolation.gif")
+    gif(anim,"SMP_$(typeof(problem))_$(problem.type)_Q_$(Q)_interpolation.gif")
 end
 end
 
-problem = Slack("TKE"; parameters=TKEMassFlux.TKEParameters())
-Q = 1
-hyp = [0.18402255361790298, 0.0034913174201650103, 0.001675888689295688]
-
-problem = Slack("KPP"; parameters=KPP.Parameters())
-Q = 1
-hyp = [0.21342375476405787, 0.03326860797873657, 0.00017668846294285946]
-
-𝒢 = LearnConvection.GaussianProcess.model(𝒟_train; kernel=LearnConvection.GaussianProcess.SpectralMixtureProductI(hyp))
-anim = animate_profile_and_model_output(𝒢, 𝒟_test)
-
-𝒟_train = LearnConvection.Data.data(train, problem; D=32, N=4);
-𝒟_test = LearnConvection.Data.data(test, problem; D=32, N=4);
-
-gif(anim,"here$(typeof(problem))_$(problem.type)_Q_$(Q)_interpolation.gif")
+# problem = Slack("TKE"; parameters=TKEMassFlux.TKEParameters())
+# Q = 1
+# hyp = [0.18402255361790298, 0.0034913174201650103, 0.001675888689295688]
+#
+# problem = Slack("KPP"; parameters=KPP.Parameters())
+# Q = 1
+# hyp = [0.21342375476405787, 0.03326860797873657, 0.00017668846294285946]
+#
+# 𝒢 = LearnConvection.GaussianProcess.model(𝒟_train; kernel=LearnConvection.GaussianProcess.SpectralMixtureProductI(hyp))
+# anim = animate_profile_and_model_output(𝒢, 𝒟_test)
+#
+# 𝒟_train = LearnConvection.Data.data(train, problem; D=32, N=4);
+# 𝒟_test = LearnConvection.Data.data(test, problem; D=32, N=4);
+#
+# gif(anim,"here$(typeof(problem))_$(problem.type)_Q_$(Q)_interpolation.gif")
